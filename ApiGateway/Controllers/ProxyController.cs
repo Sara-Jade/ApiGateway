bb3cc94e053244988341e084649c6ec1ff7f2f26
Super Juicer <you@example.com>
2022-08-16 21:02:03 -0700

    Added ProxyController to connect to AuthorsService and BooksService. Added the HTTPClient service to startup, so we can call the AuthorsService and BooksService.

---
+++ ApiGateway/Controllers/ProxyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Controllers
+{
+    [Route("[action]")]
+    [ApiController]
+    public class ProxyController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        public ProxyController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Books()
+            => await ProxyTo("https://localhost:44302/books");
+        [HttpGet]
+        public async Task<IActionResult> Authors()
+            => await ProxyTo("https://localhost:44395/authors");
+        private async Task<ContentResult> ProxyTo(string url)
+            => Content(await _httpClient.GetStringAsync(url));
+    }
+}
